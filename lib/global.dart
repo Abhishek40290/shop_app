48e722f6060a82eaa90a8ebce3e06738e340d608
Abhishek Kumar <abhishek40290@gmail.com>
2023-08-15 14:31:34 +0530

    first commit

---
+++ lib/global.dart
@@ -0,0 +1,45 @@
+final product = [
+  {
+    'id': '0',
+    'title': 'Men\'s Nike Shoes',
+    'price': 44.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/shoes_1.png',
+    'company': 'Nike',
+  },
+  {
+    'id': '0',
+    'title': 'Addidas Shoes',
+    'price': 44.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/shoes_1.png',
+    'company': 'Adidas',
+  },
+  {
+    'id': '0',
+    'title': 'Bata Shoes',
+    'price': 44.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/shoes_1.png',
+    'company': 'Bata',
+  },
+  {
+    'id': '0',
+    'title': 'Jordan Shoes',
+    'price': 44.52,
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/shoes_1.png',
+    'company': 'Nike',
+  },
+];
+
+final carat = [
+  {
+    'id': '3',
+    'title': 'Jordan Shoes',
+    'price': 420.69,
+    'imageUrl': 'assets/images/shoes_4.png',
+    'company': 'Nike',
+    'size': 8,
+  }
+];
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromRGBO(254, 206, 1, 1),
+          primary: Color.fromRGBO(254, 206, 1, 1),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          hintStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+          prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
